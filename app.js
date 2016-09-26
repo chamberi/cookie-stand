2d31514a8cbf9645d8ff1faf32941cf780d16997
Colin Lamont <colinlamont@gmail.com>
2016-09-26 16:10:23 -0700

    added total to js and html

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 var firstandpike = document.getElementById('firstandpike');
+var fandp = document.getElementById('fandp');
+var totalCookies = 0;
 
 var firstAndPike = {
   name: 'First and Pike',
@@ -25,11 +27,16 @@ var firstAndPike = {
     for (var j = 0; j < hours.length; j++) {
       this.cookiesSoldByHour.push(Math.round(this.avgCookieSoldPerHour * this.randCustByHour[j]));
       console.log(this.cookiesSoldByHour[j]);
+      totalCookies += this.cookiesSoldByHour[j];
     }
   }
 };
 firstAndPike.calcRandCustByHour();
 firstAndPike.calcCookiesSoldByHour();
+
+var h3El = document.createElement('h3');
+h3El.textContent = firstAndPike.name;
+fandp.appendChild(h3El);
 
 for (var k = 0; k < hours.length; k++) {
   var liEl = document.createElement('li');
@@ -37,3 +44,7 @@ for (var k = 0; k < hours.length; k++) {
   console.log(liEl);
   firstandpike.appendChild(liEl);
 }
+var liToEl = document.createElement('li');
+console.log(totalCookies);
+liToEl.textContent = 'Total: ' + totalCookies + ' cookies';
+firstandpike.appendChild(liToEl);
