646fe3526d371626629de6317f509d9c727cd68a
Colin Lamont <colinlamont@gmail.com>
2016-09-26 14:26:31 -0700

    Added simple html structure, initial random methods in js

---
+++ app.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+
+var firstAndPike = {
+  name: 'First and Pike',
+  minCustPerHour: 23,
+  maxCustPerHour: 65,
+  avgCookieSoldPerHour: 6.3,
+  randCustByHour: [],
+  cookiesSoldByHour: [],
+
+  // methods
+
+  // method for random  customers by hour
+  calcRandCustByHour: function() {
+    for (var i = 0; i < hours.length; i++) {
+      this.randCustByHour.push(Math.floor(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1)) + this.minCustPerHour);
+      console.log(this.randCustByHour[i]);
+    }
+  }
+  // method for cookies sold by hour
+  calcCookiesSoldByHour: function() {
+    for (var j = 0; j < hours.length; j++) {
+      this.cookiesSoldByHour.push(avgCookieSoldPerHour * randCustByHour[j]);
+      console.log(this.cookiesSoldByHour[j]);
+    }
+  }
+};
+calcRandCustByHour();
+calcCookiesSoldByHour();
